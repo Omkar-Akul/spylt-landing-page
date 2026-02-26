d61bd5a7ecc9e02697ab11c9415393d9811b8989
Omkar-Akul <omkarakul1907@gmail.com>
2026-02-26 12:28:54 +0530

    setup

--- SPYLT/src/App.jsx
+++ SPYLT/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className=' border border-red-500 text-xl'>App</div>
+    <div className='border-red-500 text-8xl'>App</div>
   )
 }
 
