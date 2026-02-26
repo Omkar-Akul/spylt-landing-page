860fd5685952629b9203b1a96fa5c48052a5cf4a
Omkar-Akul <omkarakul1907@gmail.com>
2026-02-26 13:58:04 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
 
 function App() {
   return (
+  <div>
    <Navbar />
-  )
-}
+
+   <HeroSection/>
+   </div>
+  );
+};
 
 export default App
