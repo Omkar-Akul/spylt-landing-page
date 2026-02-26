05475f5adfe3f0816ad7bf48b7ae4f0859b334b8
Omkar-Akul <omkarakul1907@gmail.com>
2026-02-26 14:04:35 +0530

    up

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <section className="bg-main-bg">
       <div className="hero-container">
-        <img src="../images/static-img.png" alt="" className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-100" />
+        <img src="../images/static-img.png" alt="" className="absolute -bottom-18 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-100" />
         <div className="hero-content">
           <div className="overflow-hidden">
             <h1 className="hero-title">Freaking Delicious </h1>
