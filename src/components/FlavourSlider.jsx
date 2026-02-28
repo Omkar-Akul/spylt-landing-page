407ca80ef6be6ff371ba8f4b51fe0f43dbaa38ee
Omkar-Akul <omkarakul1907@gmail.com>
2026-02-28 21:46:58 +0530

    media queries

--- src/components/FlavourSlider.jsx
+++ src/components/FlavourSlider.jsx
@@ -3,51 +3,79 @@ import { flavorlists } from "../constants";
 import gsap from "gsap";
 import { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
+
+gsap.registerPlugin(ScrollTrigger);
 
 const FlavourSlider = () => {
-    const sliderRef = useRef();
-  
-    const isTablet = useMediaQuery({
-      query:"(max-width-1024px)"
-    })
+  const sliderRef = useRef(null);
 
-  useGSAP (() => {
-    const scrollAmount = sliderRef.current.scrollWidth - window.innerWidth;
+  useGSAP (()=> {
+      const titleTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".flavour-section",
+        start: "top top",
+        end: "bottom 80%",
+        scrub: true,
+      },
+    });
 
-    if (!isTablet) {
-           const tl = gsap.timeline({
+    titleTl
+      .to(".first-text-split", {
+        xPercent: -30,
+        ease: "power1.inOut",
+      })
+      .to(
+        ".flavour-text-scroll",
+        {
+          xPercent: -22,
+          ease: "power1.inOut",
+        },
+        "<"
+      )
+      .to(
+        ".second-text-split",
+        {
+          xPercent: -10,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+  });
+  useGSAP(() => {
+    const mm = gsap.matchMedia();
+
+    // ✅ ONLY LAPTOP / DESKTOP
+    mm.add("(min-width: 1024px)", () => {
+      const scrollAmount =
+        sliderRef.current.scrollWidth - window.innerWidth;
+
+      const tl = gsap.timeline({
         scrollTrigger: {
           trigger: ".flavour-section",
-          start: "2% top",
-          end: `+=${scrollAmount + 1500}px`,
+          start: "top top",
+          end: `+=${scrollAmount + 1500}`,
           scrub: true,
-          pin:true,
-          markers:true,
+          pin: true,
+         
         },
       });
 
       tl.to(".flavour-section", {
-        x:  `-${scrollAmount + 1100}px`,
+        x: -(scrollAmount + 1100),
         ease: "power1.inOut",
-  });
-  
-    }
-    if(isTablet) {
-      ScrollTrigger.getAll().forEach(st => st.kill());
-      gsap.set(sliderRef.current,{
-        clear:"all",
-        x:0,
       });
-      scrollTrigger.refresh();
-    }
- 
-});
+    });
+
+    // cleanup
+    return () =>{ mm.kill(); };
+  }, []);
 
 
   return (
-    <div ref={sliderRef} className="slider-wrapper">
+    <div ref={sliderRef} className="slider-wrapper ">
       <div className="flavors">
         {
           flavorlists.map((flavor) => (
