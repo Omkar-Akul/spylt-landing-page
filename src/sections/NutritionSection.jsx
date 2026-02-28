9472abc269658a00065b49c5d2f4c07b39f1eb36
Omkar-Akul <omkarakul1907@gmail.com>
2026-02-28 22:07:42 +0530

    up

--- src/sections/NutritionSection.jsx
+++ src/sections/NutritionSection.jsx
@@ -5,7 +5,7 @@ const NutritionSection = () => {
       <img src="/images/slider-dip.png" alt="" className="w-full object-cover" />
       <img src="/images/big-img.png" alt="" className="big-img" />
       <div className="flex md:flex-row flex-col justify-between md:px-10 px-5 mt-14 md:mt-0">
-        <div className=""></div>
+        <div className="relative inline-block"></div>
       </div>
     </section>
   );
