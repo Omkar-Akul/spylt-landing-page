102d706bdae1b142d0229ffdf6a144f02ab9b614
Omkar-Akul <omkarakul1907@gmail.com>
2026-03-02 18:59:09 +0530

    Revert "up"
    This reverts commit 1bac953192f1de3ac3a4e14583c398f1524bc8d2.

--- src/sections/BenefitSection.jsx
+++ src/sections/BenefitSection.jsx
@@ -1,56 +1,98 @@
 import { useGSAP } from "@gsap/react";
+import ClipPathTitle from "../components/ClipPathTitle";
 import gsap from "gsap";
-import { useMediaQuery } from "react-responsive";
+import VideoPinSection from "../components/VideoPinSection";
 
-const VideoPinSection = () => {
-  const isMobile = useMediaQuery({
-    query: "(max-width: 768px)",
-  });
-
+const BenefitSection = () => {
   useGSAP(() => {
-    if (!isMobile) {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".vd-pin-section",
-          start: "-15% top",
-          end: "200% top",
-          scrub: 1.5,
-          pin: true,
-        },
-      });
+    const revealTl = gsap.timeline({
+      delay: 1,
+      scrollTrigger: {
+        trigger: ".benefit-section",
+        start: "top 60%",
+        end: "top top",
+        scrub: 1.5,
+      },
+    });
 
-      tl.to(".video-box", {
-        clipPath: "circle(100% at 50% 50%)",
-        ease: "power1.inOut",
+    revealTl
+      .to(".benefit-section .first-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .second-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .third-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .fourth-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
       });
-    }
   });
 
   return (
-    <section className="vd-pin-section">
-      <div
-        style={{
-          clipPath: isMobile
-            ? "circle(100% at 50% 50%)"
-            : "circle(6% at 50% 50%)",
-        }}
-        className="size-full video-box"
-      >
-        <video src="/videos/pin-video.mp4" playsInline muted loop autoPlay />
+    <section className="benefit-section">
+      <div className="container mx-auto pt-20">
+        <div className="col-center">
+          <p>
+            Unlock the Advantages: <br />
+            Explore the Key Benefits of Choosing SPYLT
+          </p>
 
-        <div className="abs-center md:scale-100 scale-200">
-          <img src="/images/circle-text.svg" alt="" className="spin-circle" />
-          <div className="play-btn">
-            <img
-              src="/images/play.svg"
-              alt=""
-              className="size-[3vw] ml-[.5vw]"
+          <div className="mt-20 col-center">
+            <ClipPathTitle
+              title={"Shelf stable"}
+              color={"#faeade"}
+              bg={"#c88e64"}
+              className={"first-title"}
+              borderColor={"#222123"}
+            />
+            <ClipPathTitle
+              title={"Protein + Caffeine"}
+              color={"#222123"}
+              bg={"#faeade"}
+              className={"second-title"}
+              borderColor={"#222123"}
+            />
+            <ClipPathTitle
+              title={"Infinitely recyclable"}
+              color={"#faeade"}
+              bg={"#7F3B2D"}
+              className={"third-title"}
+              borderColor={"#222123"}
+            />
+            <ClipPathTitle
+              title={"Lactose free"}
+              color={"#2E2D2F"}
+              bg={"#FED775"}
+              className={"fourth-title"}
+              borderColor={"#222123"}
             />
           </div>
+
+          <div className="md:mt-0 mt-10">
+            <p>And much more ...</p>
+          </div>
         </div>
+      </div>
+
+      <div className="relative overlay-box">
+        <VideoPinSection />
       </div>
     </section>
   );
 };
 
-export default VideoPinSection;
+export default BenefitSection;
