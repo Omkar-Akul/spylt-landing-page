1bac953192f1de3ac3a4e14583c398f1524bc8d2
Omkar-Akul <omkarakul1907@gmail.com>
2026-03-02 18:30:56 +0530

    up

--- src/components/VideoPinSection.jsx
+++ src/components/VideoPinSection.jsx
@@ -1,59 +1,53 @@
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { useMediaQuery } from "react-responsive";
 
 const VideoPinSection = () => {
-
-
+  
 
   useGSAP(() => {
     
       const tl = gsap.timeline({
-      scrollTrigger:{
-        trigger:".vd-pin-section",
-        start:"-15% top",
-        end:"+=180%",
-        scrub:1.5,
-        pin:true,
-        pinSpacing:false,
-      },
-    });
-    tl.to({}, {duration:4});
+        scrollTrigger: {
+          trigger: ".vd-pin-section",
+          start: "-15% top",
+          end: "200% top",
+          scrub: 1.5,
+          pin: true,
+        },
+      });
 
-    tl.fromTo(".testimonials-section",
-      {yPercent:10},
-      {yPercent:0, ease:"none", duration:1}
-    );
-
-      tl.fromTo(
-      ".video-box",
-      {clipPath:"circle(6% at 50% 50%)"},
-      {clipPath:"circle(100% at 50% 50%)",
-        ease:"none",
-        duration:2,
-      },
-      0
-      );
+      tl.to(".video-box", {
+        clipPath: "circle(100% at 50% 50%)",
+        ease: "power1.inOut",
+      });
     
-  
   });
 
-  return <section className="vd-pin-section">
-        <div style={{
-          clipPath: 
-           
+  return (
+    <section className="vd-pin-section">
+      <div
+        style={{
+          clipPath:
             "circle(6% at 50% 50%)",
-        }} className="size-full video-box">
-          <video src="/videos/pin-video.mp4" playsInline muted loop autoPlay />
-          <div className="abs-center md:scale-100 scale-200">
-            <img src="/images/circle-text.svg" alt="" className="spin-circle" />
-            <div className="play-btn">
-              <img src="/images/play.svg" alt="" className="size-[3vw] ml-[.5vw]" />
-            </div>
+        }}
+        className="size-full video-box"
+      >
+        <video src="/videos/pin-video.mp4" playsInline muted loop autoPlay />
+
+        <div className="abs-center md:scale-100 scale-200">
+          <img src="/images/circle-text.svg" alt="" className="spin-circle" />
+          <div className="play-btn">
+            <img
+              src="/images/play.svg"
+              alt=""
+              className="size-[3vw] ml-[.5vw]"
+            />
           </div>
         </div>
-      </section>
-  
-}
+      </div>
+    </section>
+  );
+};
 
-export default VideoPinSection
+export default VideoPinSection;
